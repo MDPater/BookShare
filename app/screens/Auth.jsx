69f5b9195616fe5b4cca0f91848e161cacf9d67f
Max de Pater <depatermax@gmail.com>
2025-08-21 16:35:00 +0200

    added react paper styling

--- app/screens/Auth.jsx
+++ app/screens/Auth.jsx
@@ -1,37 +1,40 @@
-import React, { useState } from 'react'
-import { Button, TextInput } from 'react-native';
-import { Alert, StyleSheet, View } from 'react-native'
-import { supabase } from '../utils/supabase'
+import React, { useState } from "react";
+import { Button, TextInput } from "react-native-paper";
+import { FontAwesome } from "@expo/vector-icons";
+import { Alert, StyleSheet, View } from "react-native";
+import { supabase } from "../utils/supabase";
+import GoogleSSO from "../components/auth/GoogleSSO";
 
 export default function Auth() {
-  const [email, setEmail] = useState('')
-  const [password, setPassword] = useState('')
-  const [loading, setLoading] = useState(false)
+  const [email, setEmail] = useState("");
+  const [password, setPassword] = useState("");
+  const [loading, setLoading] = useState(false);
 
   async function signInWithEmail() {
-    setLoading(true)
+    setLoading(true);
     const { error } = await supabase.auth.signInWithPassword({
       email: email,
       password: password,
-    })
+    });
 
-    if (error) Alert.alert(error.message)
-    setLoading(false)
+    if (error) Alert.alert(error.message);
+    setLoading(false);
   }
 
   async function signUpWithEmail() {
-    setLoading(true)
+    setLoading(true);
     const {
       data: { session },
       error,
     } = await supabase.auth.signUp({
       email: email,
       password: password,
-    })
+    });
 
-    if (error) Alert.alert(error.message)
-    if (!session) Alert.alert('Please check your inbox for email verification!')
-    setLoading(false)
+    if (error) Alert.alert(error.message);
+    if (!session)
+      Alert.alert("Please check your inbox for email verification!");
+    setLoading(false);
   }
 
   return (
@@ -39,32 +42,61 @@ export default function Auth() {
       <View style={[styles.verticallySpaced, styles.mt20]}>
         <TextInput
           label="Email"
-          leftIcon={{ type: 'font-awesome', name: 'envelope' }}
+          left={
+            <TextInput.Icon
+              icon={() => <FontAwesome name="envelope" size={20} />}
+            />
+          }
           onChangeText={(text) => setEmail(text)}
           value={email}
           placeholder="email@address.com"
-          autoCapitalize={'none'}
+          autoCapitalize={"none"}
         />
       </View>
       <View style={styles.verticallySpaced}>
         <TextInput
           label="Password"
-          leftIcon={{ type: 'font-awesome', name: 'lock' }}
+          left={
+            <TextInput.Icon
+              icon={() => <FontAwesome name="lock" size={20} />}
+            />
+          }
           onChangeText={(text) => setPassword(text)}
           value={password}
           secureTextEntry={true}
           placeholder="Password"
-          autoCapitalize={'none'}
+          autoCapitalize={"none"}
         />
       </View>
       <View style={[styles.verticallySpaced, styles.mt20]}>
-        <Button title="Sign in" disabled={loading} onPress={() => signInWithEmail()} />
+        <Button
+          mode="contained" // Use 'contained' for a filled button
+          loading={loading} // `loading` prop directly corresponds to your `loading` state
+          disabled={loading} // The `disabled` prop works the same way
+          onPress={() => signInWithEmail()}
+        >
+          Sign in
+        </Button>
       </View>
       <View style={styles.verticallySpaced}>
-        <Button title="Sign up" disabled={loading} onPress={() => signUpWithEmail()} />
+        <Button
+          mode="contained" // Use 'contained' for a filled button
+          loading={loading} // `loading` prop directly corresponds to your `loading` state
+          disabled={loading} // The `disabled` prop works the same way
+          onPress={() => signUpWithEmail()}
+        >
+          Sign up
+        </Button>
+      </View>
+
+      <View style={[styles.verticallySpaced, styles.mt20]}>
+        <Divider />
+      </View>
+      <View style={styles.verticallySpaced}>
+        <GoogleSSO />
       </View>
     </View>
-  )
+  );
 }
 
 const styles = StyleSheet.create({
@@ -75,9 +107,9 @@ const styles = StyleSheet.create({
   verticallySpaced: {
     paddingTop: 4,
     paddingBottom: 4,
-    alignSelf: 'stretch',
+    alignSelf: "stretch",
   },
   mt20: {
     marginTop: 20,
   },
-})
+});
